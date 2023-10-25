2382bebca183b5fc00d67800f11d720d76a2287b
Elena Pozhydaieva <epozhydaieva@gmail.com>
2023-10-25 13:23:47 -0400

    add conftest.py and change test_cart.py

--- lesson1/test_cart.py
+++ lesson1/test_cart.py
@@ -1,26 +1,4 @@
-import time
-
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
-@pytest.fixture()
-def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    print('\nquit browser...')
-    driver.quit()
-
-@pytest.fixture()
-def login(driver):
-    driver.get("https://www.saucedemo.com/")
-    username_field = driver.find_element(By.XPATH, '//input[@data-test="username"]')
-    username_field.send_keys("standard_user")
-    password_field = driver.find_element(By.XPATH, '//input[@data-test="password"]')
-    password_field.send_keys("secret_sauce")
-    login_button = driver.find_element(By.XPATH, '//input[@data-test="login-button"]')
-    login_button.click()
 
 
 # Adding a product to the cart through the catalog
@@ -33,7 +11,6 @@ def test_add_to_cart_through_catalog(driver, login):
     driver.find_element(By.CSS_SELECTOR, '#add-to-cart-test\.allthethings\(\)-t-shirt-\(red\)').click()
     count_cart_items = driver.find_element(By.CSS_SELECTOR, '.shopping_cart_badge')
     assert count_cart_items.text == "6"
-
 
 
 # Removing an item from your cart using the cart
@@ -57,7 +34,6 @@ def test_remove_from_cart(driver, login):
     assert len(cart_items) == 0
 
 
-
 # Adding a product to the cart from the product card
 def test_add_to_cart_from_product_card(driver, login):
     driver.find_element(By.XPATH, "//div[normalize-space()='Sauce Labs Backpack']").click()
@@ -72,7 +48,6 @@ def test_add_to_cart_from_product_card(driver, login):
 # Removing an item from the cart using the product card
 def test_remove_from_cart_using_product_card(driver, login):
     driver.find_element(By.XPATH, "//div[normalize-space()='Sauce Labs Backpack']").click()
-    time.sleep(2)
     add_to_cart_button = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']")
     add_to_cart_button.click()
     driver.find_element(By.CSS_SELECTOR, '#remove-sauce-labs-backpack').click()
